a610a5c0874e38e8ed2692e67ca7a23c9e8a55a3
ibrahimKanber <ikanber45@gmail.com>
2020-09-02 12:44:59 +0200

    responsive and script.js

---
+++ dist/script.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var searchbuton = document.getElementById("btn1");
+searchbuton.addEventListener("click", function () {
+  alert("Belki kurs bitiminde :)))");
+});
---
+++ script.js
@@ -0,0 +1,4 @@
+const searchbuton = document.getElementById("btn1");
+searchbuton.addEventListener("click", () => {
+    alert("Belki kurs bitiminde :)))")
+})
